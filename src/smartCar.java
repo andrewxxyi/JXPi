410fb3359083537d967e1d249df1adec395fb9a7
Andrew Xu <andrew@ijingxi.cn>
2016-06-10 22:24:26 +0800

    v0.2.0

--- src/smartCar.java
+++ src/smartCar.java
@@ -1,7 +1,7 @@
 import cn.ijingxi.ServerCommon.httpServer.jxHttpRes;
 import cn.ijingxi.ServerCommon.httpServer.jxHttpServer;
 import cn.ijingxi.app.ObjTag;
-import cn.ijingxi.app.People;
+import cn.ijingxi.app.Right;
 import cn.ijingxi.data.JdbcSqlite.JdbcSqlite;
 import cn.ijingxi.live.Medium;
 import cn.ijingxi.orm.JdbcUtils;
@@ -17,33 +17,27 @@ public class smartCar {
 
     public static void main(String args[]) throws Exception {
 
-        if(ObjTag.SystemID==null){
+        if (ObjTag.SystemID == null) {
             //尚未初始化
             jxLog.logger.debug("Init");
-            JdbcSqlite db=null;
+            JdbcSqlite db = null;
             try {
                 db = new JdbcSqlite("main");
                 JdbcUtils.SetDB(db);
 
                 utils.Init();
-                if(ObjTag.System==null)
-                {
-                    //系统尚未建库
-                    jxLog.logger.debug("CreateDBTable");
-                    utils.CreateDBTable();
-                    People p=People.createPeople("admin","管理员");
-                    p.Insert();
-                }
 
+                utils.CreateDBTable();
+                Right.reset();
 
                 jxHttpRes.InitResClass(Medium.class);
 
                 jxLog.logger.debug("wheel init");
                 wheel.init();
 
-                jxHttpServer server1=new jxHttpServer(10000, "./web_videojs/", null);
+                jxHttpServer server1 = new jxHttpServer(10000, "./web_videojs/", null);
                 server1.start();
-                jxHttpServer server2=new jxHttpServer(10002, "./web_html5/", null);
+                jxHttpServer server2 = new jxHttpServer(10002, "./web_html5/", null);
                 server2.start();
 
             } catch (Exception e) {
