7d89ee010ebf886eeb35555044868a84b9ee4abd
Andrew Xu <andrew@ijingxi.cn>
2016-08-17 18:01:01 +0800

    v0.7.1

--- conf/system.lua
+++ conf/system.lua
@@ -1,6 +1,6 @@
 function exec()
 	local table={
-		system={myName="MainServer",ipDev="eth0",vDepServerPort=0,WebServerPort=10002,NoLogin=false,HttpAccessLua=true,FrontPollingInterval=60,NodeMCUPort=0}
+		system={myName="MainServer",ipDev="eth0",vDepServerPort=0,WebServerPort=10002,NoLogin=false,HttpAccessLua=true,FrontPollingInterval=0,NodeMCUPort=0}
 	}
 	return table
 end
